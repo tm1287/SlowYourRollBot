c4f19a562b6ae77cfab4f194c1e9fae7eae43f9d
tm1287 <tejas.maraliga@gmail.com>
2018-08-16 21:28:39 -0400

    Read unread mentions and scrape vid

--- bot.py
+++ bot.py
@@ -1,16 +1,17 @@
 import praw
 import secret
+import time
 import urllib
 
 reddit = praw.Reddit(client_id = secret.client_id, client_secret = secret.client_secret, user_agent = secret.user_agent, username = secret.username, password = secret.password)
 
-url = "https://www.reddit.com/r/FortNiteBR/comments/95t737/how_to_counter_spray_meta/".split("/")
-
-print(url[6])
-
 def ScrapeVideo(link_id):
     submission = reddit.submission(id=link_id)
-    urllib.request.urlretrieve(str(submission.media['reddit_video']['fallback_url']), link_id + '.mp4')
+    urllib.request.urlretrieve(str(submission.media['reddit_video']['fallback_url']), link_id + '_' + str(time.time()) + '.mp4')
 
-ScrapeVideo(url[6])
 
+
+for mention in reddit.inbox.mentions(limit=25):
+    if mention.new:
+        ScrapeVideo(str(mention.submission))
+        mention.mark_read()
--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import time
 import praw
 import secret
 
@@ -8,3 +7,6 @@ reddit = praw.Reddit(client_id = secret.client_id, client_secret = secret.client
 
 for mention in reddit.inbox.mentions(limit=25):
     print('{}\n{}\n'.format(mention.author, mention.body))
+    print(mention.submission)
+    #submission = reddit.submission(str(mention.submission))
+    #print(submission.permalink.split('/')[6])
