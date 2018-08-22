17e0cb34778a206e67f246b153ca3d0c8d5e6b55
tm1287 <tejas.maraliga@gmail.com>
2018-08-21 21:11:29 -0400

    Add support for scraping videos hosted on imgur

--- bot.py
+++ bot.py
@@ -2,13 +2,17 @@ import praw
 import secret
 import os
 import urllib
+import requests
 from moviepy.editor import *
 
 reddit = praw.Reddit(client_id = secret.client_id, client_secret = secret.client_secret, user_agent = secret.user_agent, username = secret.username, password = secret.password)
 
 def ScrapeVideo(link_id, mention_id):
     submission = reddit.submission(id=link_id)
-    urllib.request.urlretrieve(str(submission.media['reddit_video']['fallback_url']), link_id + '_' + mention_id + '.mp4')
+    if submission.is_reddit_media_domain == "True":
+        urllib.request.urlretrieve(str(submission.media['reddit_video']['fallback_url']), link_id + '_' + mention_id + '.mp4')
+    else:
+        urllib.request.urlretrieve('https://imgur.com/download/' + str(str(submission.url).split('/')[3].split('.')[0]), link_id + '_' + mention_id + '.mp4')
 
 def ProcessVideo(link, mention, start_time, end_time, slow_factor):
     fullclip = VideoFileClip(link + "_" + mention + ".mp4")
@@ -22,14 +26,19 @@ def ProcessVideo(link, mention, start_time, end_time, slow_factor):
     clip2.reader.close()
     clip3.reader.close()
     os.remove(link + "_" + mention + ".mp4")
+
+#def UploadVideo(videoName):
+
 def get_sec(time_str):
     m, s = time_str.split(':')
     return int(m) * 60 + int(s)
 
-while True:
-    for mention in reddit.inbox.mentions(limit=25):
-        if mention.new:
-            ScrapeVideo(str(mention.submission), str(mention))
-            print(mention.body.split(" ")[1], mention.body.split(" ")[2], mention.body.split(" ")[3])
-            ProcessVideo(str(mention.submission), str(mention), mention.body.split(" ")[1], mention.body.split(" ")[2], mention.body.split(" ")[3])
-            mention.mark_read()
+#while True:
+#    for mention in reddit.inbox.mentions(limit=25):
+#        if mention.new:
+#            ScrapeVideo(str(mention.submission), str(mention))
+#            print(mention.body.split(" ")[1], mention.body.split(" ")[2], mention.body.split(" ")[3])
+#            ProcessVideo(str(mention.submission), str(mention), mention.body.split(" ")[1], mention.body.split(" ")[2], mention.body.split(" ")[3])
+#            mention.mark_read()
+
+ScrapeVideo("996hiv", "837hia")
--- test.py
+++ test.py
@@ -1,12 +1,10 @@
 import praw
 import secret
-
+import pprint
 
 reddit = praw.Reddit(client_id = secret.client_id, client_secret = secret.client_secret, user_agent = secret.user_agent, username = secret.username, password = secret.password)
 
-
-for mention in reddit.inbox.mentions(limit=25):
-    print('{}\n{}\n'.format(mention.author, mention.body))
-    print(mention.submission)
-    #submission = reddit.submission(str(mention.submission))
-    #print(submission.permalink.split('/')[6])
+submission = reddit.submission(id='996hiv')
+#submission = reddit.submission(id='995eot')
+print(submission.title) # to make it non-lazy
+pprint.pprint(vars(submission))
