c32695aaff79c6181972b4006fe8b5e1f70afeb7
tm1287 <tejas.maraliga@gmail.com>
2019-04-04 00:29:57 -0400

    Fix imgur scraping and ffmpeg ghost process error

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import praw
 import secret
-import os
 import urllib
+import re
+import psutil
+
+from bs4 import BeautifulSoup
 from pystreamable import StreamableApi
 from moviepy.editor import *
 
@@ -9,9 +12,19 @@ api = StreamableApi(secret.email, secret.streamable_pass)
 
 reddit = praw.Reddit(client_id = secret.client_id, client_secret = secret.client_secret, user_agent = secret.user_agent, username = secret.username, password = secret.password)
 
+#print(reddit.read_only)
+
 def ScrapeImgurVideo(link_id, mention_id):
     submission = reddit.submission(id=link_id)
-    urllib.request.urlretrieve('https://imgur.com/download/' + str(str(submission.url).split('/')[3].split('.')[0]), link_id + '_' + mention_id + '.mp4')
+    print(submission.media['oembed']['url'])
+    imgurUrl = str(submission.media['oembed']['url'])
+    imgurPage = urllib.request.urlopen(imgurUrl)
+    imgurSoup = BeautifulSoup(imgurPage, 'html.parser')
+
+    videoContainer = imgurSoup.find('meta', attrs={'property': 'og:video'})
+    videoLink = re.findall('"([^"]*)"', str(videoContainer))[0]
+
+    urllib.request.urlretrieve(videoLink, str(link_id) + '_' + str(mention_id) + '.mp4')
 
 def ScrapeRedditVideo(link_id, mention_id):
     submission = reddit.submission(id=link_id)
@@ -19,7 +32,7 @@ def ScrapeRedditVideo(link_id, mention_id):
 
 
 def ScrapeYoutubeVideo(link_id, mention_id):
-    PostReply(mention_id, "Sorry, SlowYourRollBot doesn't work on Youtube videos. This might change in the future.")
+    PostReply(mention_id, "Sorry, SlowYourRollBot doesn't work on Youtube videos. This will change in the future.")
     print("DOes it get here????")
 
 def ProcessVideo(link, mention, start_time, end_time, slow_factor):
@@ -30,10 +43,16 @@ def ProcessVideo(link, mention, start_time, end_time, slow_factor):
     result = concatenate_videoclips([clip1, clip2, clip3])
     result.write_videofile(link + "_" + mention + "_processed" + ".mp4")
     fullclip.reader.close()
+
     clip1.reader.close()
     clip2.reader.close()
     clip3.reader.close()
-    os.remove(link + "_" + mention + ".mp4")
+
+    PROCNAME = 'ffmpeg-win64-v4.1.exe'
+    for proc in psutil.process_iter():
+        # check whether the process name matches
+        if proc.name() == PROCNAME:
+            proc.kill()
 
 def UploadVideo(videoName):
     api.upload_video(videoName, str(videoName))
@@ -63,10 +82,13 @@ while True:
                 print("Path to downloaded video was not found. MoviePy Error")
             try:
                 PostReply(mention, "[Here is your processed video]" + '(https://streamable.com/' + UploadVideo(str(mention.submission) + "_" + str(mention) + "_processed.mp4") + ')')
+                print("Reply Posted")
             except (FileNotFoundError):
                 print("File could not be found for the Post Reply func.")
             mention.mark_read()
             try:
                 os.remove(str(str(mention.submission) + "_" + str(mention) + "_processed.mp4"))
+                os.remove(str(mention.submission) + "_" + str(mention) + ".mp4")
+                print("Files Deleted")
             except(FileNotFoundError):
                 print("File could not be found for the os remove function.")
--- test.py
+++ test.py
@@ -1,11 +1,28 @@
+import urllib
+import re
+
 import praw
 import secret
 import pprint
 
+from bs4 import BeautifulSoup
+
 reddit = praw.Reddit(client_id = secret.client_id, client_secret = secret.client_secret, user_agent = secret.user_agent, username = secret.username, password = secret.password)
 
+'''
 submission = reddit.submission(id='99dckg')
 #submission = reddit.submission(id='995eot')
 print(submission.title) # to make it non-lazy
 pprint.pprint(vars(submission))
 print(submission.is_reddit_media_domain)
+'''
+
+imgurUrl = "https://imgur.com/a/1Bpniri"
+imgurPage = urllib.request.urlopen(imgurUrl)
+imgurSoup = BeautifulSoup(imgurPage, 'html.parser')
+
+videoContainer = imgurSoup.find('meta', attrs={'property': 'og:video'})
+videoLink = re.findall('"([^"]*)"', str(videoContainer))[0]
+
+urllib.request.urlretrieve(videoLink, 'a' + '_' + 'b' + '.mp4')
+
